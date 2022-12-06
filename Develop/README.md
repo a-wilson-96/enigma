4716c193bf780aa4a1cc3a1cfa85be2437041173
Aaron Wilson <aaronmwilson96@gmail.com>
2022-12-06 13:35:23 -0900

    testing readme links

---
+++ Develop/README.md
@@ -0,0 +1,39 @@
+# Module 9
+
+## Description
+
+This module will make a readme file
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+enter 'node index.js' to begin
+
+## Usage
+
+it is used to make a readmefile for repositories
+
+
+## Credits
+
+Aaron
+
+## License
+
+UNLICENSED
+
+## Tests
+
+none
+
+## Questions
+
+myemail
+mygithub
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,111 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+const generateREADME = ({ title, description, installation, usage, license, contribution, tests, email, github }) => `# ${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+
+## Credits
+
+${contribution}
+
+## License
+
+${license}
+
+## Tests
+
+${tests}
+
+## Questions
+
+${email}
+${github}
+
+`
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter a title: ',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description: ',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Installation Instructions: ',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Usage Information: ',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'License: ',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Enter additional contributors: ',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Tests: ',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email: ',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your github username: ',
+  }
+])
+.then((answers) => {
+  const READMEPageContent = generateREADME(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
+  fs.writeFile('README.md', READMEPageContent, (err) => 
+  err ? console.log(err) : console.log("README has been generated")
+  );
+});
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
